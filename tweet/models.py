4eb4a8a6ac3bdf3bba8870030247f17e22b6e533
Cesaly Garrett <cesalygarrett@gmail.com>
2020-05-29 22:25:54 -0500

    Finished authentication, and custom user

---
+++ tweet/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class TwitItem(models.Model):
+    # title = models.CharField(max_length=30, default='')
+    # author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.body
